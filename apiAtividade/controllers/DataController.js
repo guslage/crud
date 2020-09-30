0536a686e9baeece32cde2a7f8e95c1900d27e6c
guslage <guslage157@gmail.com>
2020-09-30 11:39:04 -0300

    Criação dos controllers

---
+++ apiAtividade/controllers/DataController.js
@@ -0,0 +1,35 @@
+const db = require('../models');
+
+module.exports = {
+    async post(req, res){
+        const { temperatura, velocidade_vento, umidade, data, estacaoId } = req.body;
+        await db.dados.create({ temperatura, velocidade_vento, umidade, data, estacaoId })
+        .then(post => res.send(post))
+        .catch(err => res.send(err))
+    },
+
+    async index(req, res) { 
+        await db.dados.findAll()
+        .then(index => res.send(index))
+        .catch(err => res.send(err))
+    },
+
+    async find(req, res) {
+        await db.dados.findAll({ where: { id: req.params.id } })
+        .then(find => res.send(find))
+        .catch(err => res.send(err))
+    },
+
+    async update(req, res) {
+        const { temperatura, velocidade_vento, umidade, data, estacaoId } = req.body;
+        await db.dados.update({ temperatura, velocidade_vento, umidade, data, estacaoId }, { where: { id: req.params.id } })
+        .then(update => res.send(update))
+        .catch(err => res.send(err))
+    },
+
+    async delete(req, res) {
+        await db.dados.destroy({ where: { id: req.params.id } })
+        .then(res.send('ok'))
+        .catch(err => res.send(err))
+    }
+}
---
+++ apiAtividade/controllers/StationController.js
@@ -0,0 +1,35 @@
+const db = require('../models');
+
+module.exports = {
+    async post(req, res){
+        const { serial, lat, lon, nome } = req.body
+        await db.estacoes.create({ serial, lat, lon, nome })
+        .then(post => res.send(post))
+        .catch(err => res.send(err))
+    },
+
+    async index(req, res) { 
+        await db.estacoes.findAll()
+        .then(index => res.send(index))
+        .catch(err => res.send(err))
+    },
+
+    async find(req, res) {
+        await db.estacoes.findOne({ where: { id: req.params.id }, include: db.dados })
+        .then(find => res.send(find))
+        .catch(err => res.send(err))
+    },
+
+    async update(req, res) {
+        const { serial, lat, lon, nome } = req.body
+        await db.estacoes.update({ serial, lat, lon, nome },{ where: { id: req.params.id }})
+        .then(update => res.send(update))
+        .catch(err => res.send(err))
+    },
+
+    async delete(req, res) {
+        await db.estacoes.destroy({ where: { id: req.params.id } })
+        .then(res.send('ok'))
+        .catch(err => res.send(err))
+    }
+}
