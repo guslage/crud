ef392d74ae1d1e77c36a3827645e72b8c2bd10be
guslage <guslage157@gmail.com>
2020-09-30 11:35:57 -0300

    Queries passadas para controllers

---
+++ apiAtividade/routes/DataRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const Controller = require('../controllers/DataController');
+
+router.post('/new', Controller.post);
+router.get('/index', Controller.index);
+router.get('/find/:id', Controller.find);
+router.put('/update/:id', Controller.update);
+router.delete('/delete/:id', Controller.delete);
+
+module.exports = router;
