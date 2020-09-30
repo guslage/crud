31c825c3be45b806d705db79e8566751345966cd
guslage <guslage157@gmail.com>
2020-09-30 14:08:57 -0300

    Modificação de variáveis

--- crud/src/pages/dados/update/index.js
+++ crud/src/pages/dados/update/index.js
@@ -10,9 +10,9 @@ const darkTheme = createMuiTheme({
     },
   });
 
-export default class UpdateDado extends Component {
+export default class DataUpdate extends Component {
     state = {
-        dados: {}, 
+        data: [], 
         redirect: false
     }
 
@@ -23,33 +23,33 @@ export default class UpdateDado extends Component {
     loadList = async () => {
         const { id } = this.props.match.params;
         const response = await api.get(`/dados/find/${id}`);
-        this.setState({ dados: response.data[0] }); 
+        this.setState({ data: response.data[0] }); 
     }
 
     render(){
-        const { dados, redirect } = this.state;
+        const { data, redirect } = this.state;
 
         if(redirect){
-            return window.location.href = `/estacoes/find/${dados.estacaoId}`
+            return window.location.href = `/estacoes/find/${data.estacaoId}`
         }else{
             return(
                 <div className="MainContainer">
                 <ThemeProvider theme={darkTheme}>    
                 <form onSubmit={this.handleSubmit} autoComplete="off">
                     <div className="Text">
-                    <TextField type="number" id="temperatura" label="Temperatura" variant="filled" value={dados.temperatura} onChange={this.handleInputChange} InputLabelProps={{ style: { color: '#90CAF9' } }} inputProps={{step: 0.1}} fullWidth={true}/>
+                    <TextField type="number" id="temperatura" label="Temperatura" variant="filled" value={data.temperatura} onChange={this.handleInputChange} InputLabelProps={{ style: { color: '#90CAF9' } }} inputProps={{step: 0.1}} fullWidth={true}/>
                     </div>
                     <div className="Text">
-                    <TextField type="number" id="velocidade_vento" label="Velocidade do vento" variant="filled" value={dados.velocidade_vento} onChange={this.handleInputChange} InputLabelProps={{ style: { color: '#90CAF9' } }} inputProps={{step: 0.1}} fullWidth={true}/>
+                    <TextField type="number" id="velocidade_vento" label="Velocidade do vento" variant="filled" value={data.velocidade_vento} onChange={this.handleInputChange} InputLabelProps={{ style: { color: '#90CAF9' } }} inputProps={{step: 0.1}} fullWidth={true}/>
                     </div>
                     <div className="Text">
-                    <TextField type="number" id="umidade" label="Umidade" variant="filled" value={dados.umidade} onChange={this.handleInputChange} InputLabelProps={{ style: { color: '#90CAF9' } }} inputProps={{step: 0.1}} fullWidth={true}/>
+                    <TextField type="number" id="umidade" label="Umidade" variant="filled" value={data.umidade} onChange={this.handleInputChange} InputLabelProps={{ style: { color: '#90CAF9' } }} inputProps={{step: 0.1}} fullWidth={true}/>
                     </div>
                     <div className="Text">
-                    <TextField id="data" label="Birthday" type="date" value={dados.data} InputLabelProps={{ shrink: true, style: { color: '#90CAF9' } }} onChange={this.handleInputChange} fullWidth={true}/>
+                    <TextField id="data" label="Birthday" type="date" value={data.data} InputLabelProps={{ shrink: true, style: { color: '#90CAF9' } }} onChange={this.handleInputChange} fullWidth={true}/>
                     </div>
                     <div className="Text">
-                    <TextField type="number" id="estacaoId" label="Estação pertencente" variant="filled" value={dados.estacaoId} InputLabelProps={{ style: { color: '#90CAF9' } }} fullWidth={true}/>
+                    <TextField type="number" id="estacaoId" label="Estação pertencente" variant="filled" value={data.estacaoId} InputLabelProps={{ style: { color: '#90CAF9' } }} fullWidth={true}/>
                     </div>
                     <Button type="submit" variant="contained" color="primary">Atualizar</Button>
                 </form>  
@@ -65,15 +65,15 @@ export default class UpdateDado extends Component {
         const value = target.value;
         
         this.setState(prevState => ({
-            dados: { ...prevState.dados, [name]: value }
+            data: { ...prevState.data, [name]: value }
         }))
     }
 
     handleSubmit = event => {
         const { id } = this.props.match.params;
-        let data = this.state.dados;
+        let req = this.state.data;
         
-        api.put(`/dados/update/${id}`, data, {
+        api.put(`/dados/update/${id}`, req, {
             headers: {'content-type': 'application/json'}
         }).then(
             this.setState({redirect: true})
--- crud/src/routes/routes.js
+++ crud/src/routes/routes.js
@@ -5,8 +5,9 @@ import Estacoes from '../pages/estacoes/read/index';
 import StationFind from '../pages/estacoes/read/findOne';
 import CreateEstacao from '../pages/estacoes/create/index';
 import StationUpdate from '../pages/estacoes/update/index';
+
 import CreateDado from '../pages/dados/create/index';
-import UpdateDado from '../pages/dados/update/index';
+import DataUpdate from '../pages/dados/update/index';
 
 const Routes = () => (
     <BrowserRouter>
@@ -16,7 +17,7 @@ const Routes = () => (
             <Route exact path="/estacoes/create/" component={CreateEstacao} />
             <Route exact path="/estacoes/update/:id" component={StationUpdate} /> 
             <Route exact path="/dados/create/:estacaoId" component={CreateDado} />
-            <Route exact path="/dados/update/:id" component={UpdateDado} />
+            <Route exact path="/dados/update/:id" component={DataUpdate} />
         </Switch>
     </BrowserRouter>
 )
