66e852fcc9962a4e3cb0499289ad25e1f1609d23
guslage <guslage157@gmail.com>
2020-09-30 14:37:52 -0300

    Renomeação das rotas

--- apiAtividade/server.js
+++ apiAtividade/server.js
@@ -8,10 +8,10 @@ const db = require('./models');
 app.use(express.json());
 
 const DataRoutes = require('./routes/DataRoutes');
-app.use('/api/dados', DataRoutes);
+app.use('/api/data', DataRoutes);
 
 const StationRoutes = require('./routes/StationRoutes');
-app.use('/api/estacoes', StationRoutes);
+app.use('/api/station', StationRoutes);
 
 db.sequelize.sync().then(()=> {
     app.listen(3001, () => {
