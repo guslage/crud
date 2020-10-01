64368d881eb801f3222d13808b0f68718f7840b8
guslage <guslage157@gmail.com>
2020-10-01 10:54:17 -0300

    Atualizada a rota do componente NavBar

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Routes from './routes/routes';
-import NavBar from './navbar/navbar';
+import NavBar from './components/navbar/navbar';
 
 function App() {
   return (
