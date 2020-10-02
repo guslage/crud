0a093bc1ed01209ef2aa125006d076e6e9b4edb8
guslage <guslage157@gmail.com>
2020-10-02 02:00:52 -0300

    Adicionada rota para encontrar dados referentes a uma estação

--- apiAtividade/routes/DataRoutes.js
+++ apiAtividade/routes/DataRoutes.js
@@ -6,6 +6,7 @@ const Controller = require('../controllers/DataController');
 router.post('/new', Controller.post);
 router.get('/index', Controller.index);
 router.get('/find/:id', Controller.find);
+router.get('/findStation/:station', Controller.findStation);
 router.put('/update/:id', Controller.update);
 router.delete('/delete/:id', Controller.delete);
 
